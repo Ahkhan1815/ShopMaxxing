a749efea0cdba1b764294a7bccf0a763e303de42
Ahkhan1815 <ahkhan1815@gmail.com>
2026-02-06 19:44:27 -0600

    initial commit

---
+++ server/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def test():
+    return "test"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True, port=4000)
